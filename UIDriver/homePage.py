b7202f71fcf72a0e54b585a821f0602713b6823f
joshigwe777 <39813342+joshigwe777@users.noreply.github.com>
2023-02-19 14:33:38 -0500

    automation begun

--- UIDriver/homePage.py
+++ UIDriver/homePage.py
@@ -8,7 +8,22 @@ from webdriver_manager.chrome import ChromeDriverManager
 options = Options()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-driver.get("https://espn.com/")
+driver.get("http://lyricninja-env-1.eba-hq4sm8mv.us-east-2.elasticbeanstalk.com/")
+# driver.close()
+# driver.quit()
+
+from selenium.webdriver.common.by import By
+
+class GameBase:
+    def __init__(self, driver):
+        self.driver = driver
+        
+# need to adjust the ids and classes in the original project to make selecting a little easier
+    center = driver.find_element(By.CLASS_NAME, "center")
+    anchor_tags = center.find_elements(By.TAG_NAME, "a")
+    playButton = anchor_tags[1]
+    playButton.click()
+
 
 
 
